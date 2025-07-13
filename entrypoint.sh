5170db5adbd13f506bee1d0d377c382774ec41b8
nix <nix@local>
2025-07-13 19:10:34 +0200

    add some examples

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,11 +16,9 @@ mkdir -p "$CRYPTFS_MOUNT" "$SSHFS_MOUNT"
 SSH_KEY_FILE=$(mktemp)
 chmod 600 "$SSH_KEY_FILE"
 echo -e "$SSHFS_KEY" > "$SSH_KEY_FILE"
-cat $SSH_KEY_FILE
 
 KNOWN_HOSTS_FILE=$(mktemp)
 echo -e "$SSHFS_HOSTKEY" > "$KNOWN_HOSTS_FILE"
-cat $KNOWN_HOSTS_FILE
 
 cleanup() {
     sync
@@ -41,11 +39,11 @@ sshfs \
       "${SSHFS_URI}" \
       "$SSHFS_MOUNT"
 
-mkdir -p $ENCRYPTED_DIR
+mkdir -p "$ENCRYPTED_DIR"
 
 # Initialize gocryptfs if needed (only on first use)
 if [ ! -f "$ENCRYPTED_DIR/gocryptfs.conf" ]; then
-    echo "$GOCRYPTFS_PASSWORD" | gocryptfs -config $ENCRYPTED_DIR/gocryptfs.conf -init "$ENCRYPTED_DIR"
+    echo "$GOCRYPTFS_PASSWORD" | gocryptfs -config "$ENCRYPTED_DIR/gocryptfs.conf" -init "$ENCRYPTED_DIR"
 fi
 
 echo "$GOCRYPTFS_PASSWORD" | gocryptfs "$ENCRYPTED_DIR" "$CRYPTFS_MOUNT"
