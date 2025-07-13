e7cd2b2e72c427086180993c28b7fc58406a1127
nix <nix@local>
2025-07-13 18:46:35 +0200

    personal testings part 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,50 +1,52 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # Env Required:
 # - SSHFS_URI: SSHFS uses this as the remote location to mount.
 # - GOCRYPTFS_PASSWORD: The gocryptfs password
 # - SSHFS_HOSTKEY: Use `ssh-keyscan -p 23 xxx.your-storagebox.de` to get this
 # - SSHFS_KEY: The ssh private key file content
-#
-# Env Default:
-# - SSHFS_PORT: default 23
 set -e
 
 CRYPTFS_MOUNT="/mnt/gocryptfs"
 SSHFS_MOUNT="/mnt/sshfs"
-ENCRYPTED_DIR="$SSHFS_MOUNT/.encrypted"
+ENCRYPTED_DIR="$SSHFS_MOUNT/encrypted"
 
 mkdir -p "$CRYPTFS_MOUNT" "$SSHFS_MOUNT"
 
 SSH_KEY_FILE=$(mktemp)
 chmod 600 "$SSH_KEY_FILE"
-echo "$SSHFS_KEY" > "$SSH_KEY_FILE"
+echo -e "$SSHFS_KEY" > "$SSH_KEY_FILE"
+cat $SSH_KEY_FILE
 
 KNOWN_HOSTS_FILE=$(mktemp)
-echo "$SSHFS_HOSTKEY" > "$KNOWN_HOSTS_FILE"
+echo -e "$SSHFS_HOSTKEY" > "$KNOWN_HOSTS_FILE"
+cat $KNOWN_HOSTS_FILE
 
 cleanup() {
+    sync
     fusermount -u "$CRYPTFS_MOUNT" || true
+    sync
+    sleep 1
     fusermount -u "$SSHFS_MOUNT" || true
     rm -f "$SSH_KEY_FILE" "$KNOWN_HOSTS_FILE"
 }
 trap cleanup EXIT
 
 sshfs \
+    -o allow_other \
     -o IdentityFile="$SSH_KEY_FILE" \
     -o UserKnownHostsFile="$KNOWN_HOSTS_FILE" \
     -o StrictHostKeyChecking=yes \
-    -o port="${SSHFS_PORT:-23}" \
+    -p 23 \
       "${SSHFS_URI}" \
       "$SSHFS_MOUNT"
 
 mkdir -p $ENCRYPTED_DIR
 
 # Initialize gocryptfs if needed (only on first use)
-if [ ! -f "$SSHFS_MOUNT/gocryptfs.conf" ]; then
-    echo "$GOCRYPTFS_PASSWORD" | gocryptfs -config $SSHFS_MOUNT/gocryptfs.conf -init "$ENCRYPTED_DIR"
+if [ ! -f "$ENCRYPTED_DIR/gocryptfs.conf" ]; then
+    echo "$GOCRYPTFS_PASSWORD" | gocryptfs -config $ENCRYPTED_DIR/gocryptfs.conf -init "$ENCRYPTED_DIR"
 fi
 
 echo "$GOCRYPTFS_PASSWORD" | gocryptfs "$ENCRYPTED_DIR" "$CRYPTFS_MOUNT"
 
-sleep 30
-# python -u /app/main.py
+python -u /app/main.py
